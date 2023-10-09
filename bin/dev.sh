4a1447910b715d2031b6e385b5b3990b6f34f854
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2023-10-09 09:41:39 +0200

    start script

---
+++ bin/dev.sh
@@ -0,0 +1,13 @@
+function launch_map {
+    cd map && tegola serve --config tegola.toml
+}
+
+function launch_node {
+    cd app && npm run dev
+}
+
+function launch_django {
+    cd web && poetry run python manage.py runserver
+}
+
+(trap 'kill 0' SIGINT; launch_map & launch_node & launch_django)
