78ce0092c0841fa533f79063c205be9c44efce81
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2025-02-09 12:34:45 +0000

    reduce time between retries

--- web/parc_elec/rte.py
+++ web/parc_elec/rte.py
@@ -9,14 +9,14 @@ CLIENT_ID = os.getenv("PARC_ELEC_FR_RTE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("PARC_ELEC_FR_RTE_CLIENT_SECRET")
 
 
-def _get_access_token():
+def _get_access_token(session):
     logger.debug("Fetching access token")
 
     assert CLIENT_ID is not None, "client id is not defined"
     assert CLIENT_SECRET is not None, "client secret is not defined"
 
     auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    response = requests.post(
+    response = session.post(
         "https://digital.iservices.rte-france.com/token/oauth/", auth=auth
     ).json()
     logger.debug("Access token fetched")
@@ -24,36 +24,24 @@ def _get_access_token():
     return response["access_token"]
 
 
-def fetch_current_production_mix():
-    logger.debug("Fetching mix")
-
-    token = _get_access_token()
-    response = requests.get(
-        "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_production_type",
-        headers={"Authorization": f"Bearer {token}"},
+def _get_session():
+    session = requests.Session()
+    retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[500, 502])
+    session.mount(
+        "https://digital.iservices.rte-france.com",
+        adapter=HTTPAdapter(max_retries=retries),
     )
 
-    logger.debug(f"Response status: {response.status_code}")
-    if response.status_code != 200:
-        logger.error("Could not fetch mix data")
-        raise RuntimeError("Could not fetch mix from RTE")
+    return session
 
-    logger.debug("Mix fetched")
-
-    return response.json()
 
 
 def fetch_current_production():
     logger.debug("Fetching production data")
 
-    token = _get_access_token()
-    s = requests.Session()
-    retries = Retry(total=10, backoff_factor=0.5, status_forcelist=[500, 502])
-    s.mount(
-        "https://digital.iservices.rte-france.com",
-        adapter=HTTPAdapter(max_retries=retries),
-    )
-    response = s.get(
+    session = _get_session()
+    token = _get_access_token(session)
+    response = session.get(
         "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_unit",
         headers={"Authorization": f"Bearer {token}"},
     )
@@ -70,13 +58,9 @@ def fetch_current_production():
 def fetch_current_installed_capacity():
     logger.debug("Fetching installed data")
 
-    token = _get_access_token()
-    s = requests.Session()
-    retries = Retry(total=10, backoff_factor=0.5, status_forcelist=[500, 502])
-    s.mount(
-        "https://digital.iservices.rte-france.com", adapter=HTTPAdapter(max_retries=retries)
-    )
-    response = s.get(
+    session = _get_session()
+    token = _get_access_token(session)
+    response = session.get(
         "https://digital.iservices.rte-france.com/open_api/generation_installed_capacities/v1/capacities_per_production_unit",
         headers={"Authorization": f"Bearer {token}"},
     )
